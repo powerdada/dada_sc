f0671938852ef418cd202b1fb92d490b760390b2
Abraham Milano <amilanoa@gmail.com>
2017-09-08 14:48:31 -0400

    wip

--- contracts/DadaCollectible.sol
+++ contracts/DadaCollectible.sol
@@ -362,9 +362,9 @@ contract DadaCollectible {
     }
   }
 
-  function getDrawingsByAccount(address owner) public returns(uint[]){
+  // function getDrawingsByAccount(address owner) public returns(uint[]){
 
-  } 
+  // } 
 
   // // converts uint into string
   // function uintToString(uint v) internal returns (string) {
