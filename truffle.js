7093a87ce4a06bf4f4012d06e3fd8ff34db82ba0
Juan Gonzalez <jags84@gmail.com>
2017-09-08 10:16:22 -0400

    Clean

--- truffle.js
+++ truffle.js
@@ -6,7 +6,8 @@ module.exports = {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: '*', // Match any network id
+      gas: 4612388
     }
   }
 }
